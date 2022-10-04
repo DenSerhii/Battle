90bddf3afbe824c8b15b410a254a4d38a8713c03
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-04 17:18:22 +0200

    changes

--- Battle/src/com/company/Main.java
+++ Battle/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code
+        Battlepoint battlepoint=new Battlepoint();
+        battlepoint.ShowMap();
         System.out.println("Test");
     }
 }
