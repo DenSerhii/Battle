01957f467bb1b05dc835a96e2f72882e0e53c125
Serhii Denysenko <den.serhii@gmail.com>
2022-10-04 18:21:20 +0300

    changes2.0

--- Battle/src/com/company/Battlepoint.java
+++ Battle/src/com/company/Battlepoint.java
@@ -6,7 +6,11 @@ public class Battlepoint {
 
     public void ShowMap(){
     for (int i=0; i<10; i++) {
-        for(int j=0; j<10; j++) System.out.print("-");
+        for(int j=0; j<10; j++){
+            System.out.print("-");
+        }
+        System.out.println();
      }
+
     }
 }
