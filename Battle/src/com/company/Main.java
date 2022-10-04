3a3b3e0af6ec0866dc42b2746af024a0271cc187
Serhii Denysenko <den.serhii@gmail.com>
2022-10-04 15:56:58 +0300

    add class

--- Battle/src/com/company/Main.java
+++ Battle/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code h``      0// System.out.println("Test");
+	// write your code
+        System.out.println("Test");
     }
 }
